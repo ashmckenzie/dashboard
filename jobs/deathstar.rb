6bd79c6bf497002fa8fcf85b45730a3e3515e4ba
Ash McKenzie <ash@greenworm.com.au>
2012-11-08 08:45:43 +1100

    added memory usage graph

--- jobs/deathstar.rb
+++ jobs/deathstar.rb
@@ -1,5 +1,7 @@
-image_uri = 'http://stats.the-rebellion.net/deathstar/deathstar/cpu-day.png'
+cpu_image_uri = 'http://stats.the-rebellion.net/deathstar/deathstar/cpu-day.png'
+memory_image_uri = 'http://stats.the-rebellion.net/deathstar/deathstar/memory-day.png'
 
 SCHEDULER.every "5m", :first_in => 0 do |job|
-  send_event('server-deathstar', image: image_uri)
+  send_event('server-deathstar-cpu', image: cpu_image_uri)
+  send_event('server-deathstar-memory', image: memory_image_uri)
 end

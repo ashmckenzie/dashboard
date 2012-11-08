31bbc7704510eeed9e787ec323bd8e88a1e2974a
Ash McKenzie <ash@greenworm.com.au>
2012-11-08 21:54:52 +1100

    downloads and downloaded

--- jobs/downloads.rb
+++ jobs/downloads.rb
@@ -1,7 +1,7 @@
 DOWNLOADS_CONFIG = $APP_CONFIG.downloads
 
 SCHEDULER.every DOWNLOADS_CONFIG.refresh, :first_in => 0 do |job|
-  queue = Downloads::Sabnzbd.queue
-  data = { items: queue[:jobs] }
-  send_event('downloads', data)
+  detail = Downloads::Sabnzbd.status
+  send_event('downloads-queue', { items: detail[:queue][:jobs] })
+  send_event('downloads-history', { items: detail[:history][:jobs] })
 end
--- lib/downloads/sabnzbd.rb
+++ lib/downloads/sabnzbd.rb
@@ -2,6 +2,13 @@ module Downloads
 
   class Sabnzbd
 
+    def self.status
+      {
+        :queue => queue,
+        :history => history
+      }
+    end
+
     def self.queue
       detail = {
         apikey: $APP_CONFIG.downloads.api_key
@@ -13,7 +20,22 @@ module Downloads
       response = JSONGet.request(uri.to_s)
 
       {
-        :jobs => response.jobs.map { |job| { label: job.filename, value: job.timeleft }}
+        :jobs => response.jobs.map { |job| { label: job.filename, value: job.timeleft.gsub(/:\d+$/, '') }}
+      }
+    end
+
+    def self.history
+      detail = {
+        apikey: $APP_CONFIG.downloads.api_key
+      }
+
+      uri_options = "&apikey=%<apikey>s"
+      uri = URI.parse("#{$APP_CONFIG.downloads.base_uri}/sabnzbd/api?mode=history&output=json&#{sprintf(uri_options, detail)}")
+
+      response = JSONGet.request(uri.to_s)
+
+      {
+        :jobs => response.history.slots.map { |slot| { label: slot.name, value: '' }}
       }
     end
   end
